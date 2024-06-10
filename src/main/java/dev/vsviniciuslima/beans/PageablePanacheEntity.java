19c6be1e79a27a4fd382200149081864b59f2263
Vinicius Santana <vsviniciuslima@usp.br>
2024-06-09 21:31:07 -0300

    style: identation

--- src/main/java/dev/vsviniciuslima/beans/PageablePanacheEntity.java
+++ src/main/java/dev/vsviniciuslima/beans/PageablePanacheEntity.java
@@ -7,14 +7,11 @@ import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import io.quarkus.panache.common.Page;
 import io.quarkus.panache.common.impl.GenerateBridge;
 
-import java.util.Collections;
 import java.util.List;
 
 public abstract class PageablePanacheEntity extends PanacheEntity {
 
-    @GenerateBridge(
-            targetReturnTypeErased = true
-    )
+    @GenerateBridge(targetReturnTypeErased = true)
     public static <T extends PanacheEntityBase> List<T> page(PageRequest params, PanacheQuery query) {
         Page page = params.getPage();
         return find(query.query(), params.getSort(), query.params())
@@ -22,9 +19,7 @@ public abstract class PageablePanacheEntity extends PanacheEntity {
                 .list();
     }
 
-    @GenerateBridge(
-            targetReturnTypeErased = true
-    )
+    @GenerateBridge(targetReturnTypeErased = true)
     public static PageResponse pageResponse(PageRequest params, PanacheQuery query) {
 
         List<? extends PageablePanacheEntity>
